9b46c0ebab6dd2e286dbd73d419abb469f7ac396
黄京京 <huangjingjing@aeromedia.cn>
2025-10-15 14:19:55 +0800

    fix: update readme for style

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,17 @@
-export { SearchReplacePlugin } from './plugin';
-export * from './findReplacePlugin';
-declare const _default: {
-    SearchReplacePlugin: () => Promise<import("@tiptap/core").Extension<any, any>>;
-};
-export default _default;
+import { Extension } from "@tiptap/core";
+import { findReplacePluginKey, FindReplaceAction } from "./findReplacePlugin";
+declare module "@tiptap/core" {
+    interface Commands<ReturnType> {
+        search: {
+            find: (query: string) => ReturnType;
+            findNext: () => ReturnType;
+            findPrevious: () => ReturnType;
+            replace: (replacement: string) => ReturnType;
+            replaceAll: (replacement: string) => ReturnType;
+            closeFindReplace: () => ReturnType;
+        };
+    }
+}
+declare const SearchReplacePlugin: Extension<any, any>;
+export { SearchReplacePlugin, findReplacePluginKey };
+export type { FindReplaceAction };
--- dist/index.js
+++ dist/index.js
@@ -1,47 +1,112 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.SearchReplacePlugin = void 0;
-// 导出插件
-var plugin_1 = require("./plugin");
-Object.defineProperty(exports, "SearchReplacePlugin", { enumerable: true, get: function () { return plugin_1.SearchReplacePlugin; } });
-__exportStar(require("./findReplacePlugin"), exports);
-// 默认导出
-exports.default = {
-    SearchReplacePlugin: () => Promise.resolve().then(() => __importStar(require('./plugin'))).then(m => m.SearchReplacePlugin),
-};
+exports.findReplacePluginKey = exports.SearchReplacePlugin = void 0;
+// FindReplace.ts
+const core_1 = require("@tiptap/core");
+const findReplacePlugin_1 = require("./findReplacePlugin");
+Object.defineProperty(exports, "findReplacePluginKey", { enumerable: true, get: function () { return findReplacePlugin_1.findReplacePluginKey; } });
+const state_1 = require("@tiptap/pm/state");
+const SearchReplacePlugin = core_1.Extension.create({
+    name: "findReplace",
+    addOptions() {
+        return {
+            openPanel: "Mod-f",
+        };
+    },
+    addProseMirrorPlugins() {
+        return [(0, findReplacePlugin_1.findReplacePlugin)()];
+    },
+    addCommands() {
+        return {
+            find: (query) => ({ tr, dispatch }) => {
+                if (dispatch) {
+                    const action = { type: "FIND", query };
+                    console.log(action);
+                    tr.setMeta(findReplacePlugin_1.findReplacePluginKey, { action });
+                }
+                return true;
+            },
+            findNext: () => ({ tr, dispatch, state }) => {
+                var _a, _b;
+                if (dispatch) {
+                    const action = {
+                        type: "NAVIGATE",
+                        direction: 1,
+                    };
+                    // 传递 view 以便在插件中使用
+                    tr.setMeta("view", (_b = (_a = state.plugins.find((p) => p.spec.key === findReplacePlugin_1.findReplacePluginKey)) === null || _a === void 0 ? void 0 : _a.spec) === null || _b === void 0 ? void 0 : _b.view);
+                    tr.setMeta(findReplacePlugin_1.findReplacePluginKey, { action });
+                }
+                return true;
+            },
+            findPrevious: () => ({ tr, dispatch, state }) => {
+                var _a, _b;
+                if (dispatch) {
+                    const action = {
+                        type: "NAVIGATE",
+                        direction: -1,
+                    };
+                    tr.setMeta("view", (_b = (_a = state.plugins.find((p) => p.spec.key === findReplacePlugin_1.findReplacePluginKey)) === null || _a === void 0 ? void 0 : _a.spec) === null || _b === void 0 ? void 0 : _b.view);
+                    tr.setMeta(findReplacePlugin_1.findReplacePluginKey, { action });
+                }
+                return true;
+            },
+            replace: (replacement) => ({ state, dispatch }) => {
+                // 1. 从插件状态中获取当前的匹配信息
+                const pluginState = findReplacePlugin_1.findReplacePluginKey.getState(state);
+                if (!pluginState || pluginState.activeMatchIndex === -1) {
+                    return false; // 如果没有激活的匹配项，则不执行任何操作
+                }
+                const { from, to } = pluginState.matches[pluginState.activeMatchIndex];
+                // 2. 创建一个新的事务来执行文本替换
+                const tr = state.tr;
+                tr.insertText(replacement, from, to);
+                // 3. 更新选区，将光标移动到替换后的文本末尾
+                const newSelection = state_1.TextSelection.create(tr.doc, to + (replacement.length - (to - from)));
+                tr.setSelection(newSelection);
+                // 4. Dispatch 这个事务，这将更新编辑器的文档和视图
+                if (dispatch) {
+                    dispatch(tr);
+                }
+                // 5. (异步) 替换完成后，立即使用相同的查询词重新查找
+                // 使用 setTimeout 确保在 DOM 更新后再执行，避免竞态条件
+                setTimeout(() => {
+                    this.editor.commands.find(pluginState.query);
+                }, 0);
+                return true;
+            },
+            // ... replaceAll 命令也需要类似处理，但逻辑更简单
+            replaceAll: (replacement) => ({ state, dispatch }) => {
+                const pluginState = findReplacePlugin_1.findReplacePluginKey.getState(state);
+                if (!pluginState || pluginState.matches.length === 0) {
+                    return false;
+                }
+                const tr = state.tr;
+                // 从后往前替换，避免位置偏移
+                const reversedMatches = [...pluginState.matches].reverse();
+                reversedMatches.forEach(({ from, to }) => {
+                    tr.insertText(replacement, from, to);
+                });
+                if (dispatch) {
+                    dispatch(tr);
+                }
+                // 替换全部后，清空查找状态
+                setTimeout(() => {
+                    this.editor.commands.find("");
+                }, 0);
+                return true;
+            },
+            closeFindReplace: () => ({ tr, dispatch }) => {
+                if (dispatch) {
+                    const action = { type: "CLOSE_PANEL" };
+                    tr.setMeta(findReplacePlugin_1.findReplacePluginKey, { action });
+                }
+                setTimeout(() => {
+                    this.editor.commands.find("");
+                }, 0);
+                return true;
+            },
+        };
+    },
+});
+exports.SearchReplacePlugin = SearchReplacePlugin;
